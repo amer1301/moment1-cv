55702faa43e9304224489fc9f129ec1d3d2adc99
Amanda Persdotter <amer1301@student.miun.se>
2025-04-05 14:01:28 +0200

    railway

--- install.js
+++ install.js
@@ -10,7 +10,6 @@ const connection = mysql.createConnection({
     port: process.env.MYSQL_PORT        // Använder miljövariabler från .env
 });
 
-
 connection.connect((err) => {
     if (err) {
         console.error("Connection failed " + err);
@@ -19,33 +18,33 @@ connection.connect((err) => {
 
     console.log("connected to MySQL");
 
-// skapa databas
-connection.query("CREATE DATABASE IF NOT EXISTS cv", (err, result) => {
-    if (err) throw err;
-    console.log("Database created or exists already");
-
-    // växla till databas
-    connection.changeUser({ database: 'cv' }, (err) => {
+    // skapa databas (här används railway som databasnamn istället för cv)
+    connection.query("CREATE DATABASE IF NOT EXISTS railway", (err, result) => {
         if (err) throw err;
-        console.log("Database changed to cv");
+        console.log("Database created or exists already");
 
-// SQL-fråga
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    coursecode VARCHAR(50) NOT NULL,
-    coursename VARCHAR(255) NOT NULL,
-    syllabus VARCHAR(255),
-    progression CHAR(1),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-`;
+        // växla till databas railway
+        connection.changeUser({ database: 'railway' }, (err) => {
+            if (err) throw err;
+            console.log("Database changed to railway");
 
-connection.query(createTableQuery, (err, results) => {
-if (err) throw err;
-console.log("Table courses created or exists already");
-connection.end(); // Stäng anslutningen
-});
-});
-});
+            // SQL-fråga för att skapa table courses
+            const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS courses (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                coursecode VARCHAR(50) NOT NULL,
+                coursename VARCHAR(255) NOT NULL,
+                syllabus VARCHAR(255),
+                progression CHAR(1),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            `;
+
+            connection.query(createTableQuery, (err, results) => {
+                if (err) throw err;
+                console.log("Table courses created or exists already");
+                connection.end(); // Stäng anslutningen
+            });
+        });
+    });
 });
