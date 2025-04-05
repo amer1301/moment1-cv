aa1f467f7c154676cf62e24a938ee3b97a51739c
Amanda Persdotter <amer1301@student.miun.se>
2025-04-05 11:16:00 +0200

    testar miljövariabler igen

--- install.js
+++ install.js
@@ -1,12 +1,15 @@
+require('dotenv').config(); // Läser miljövariabler från .env-filen
+
 const mysql = require("mysql");
 
-// Anslutningsanställningar
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Amanda12",
-    port: 3307
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+
 
 connection.connect((err) => {
     if (err) {
